c66d3868473d04b5dc530ff774cba5e98627ffc8
Mortos Der Soulstealer <iAmMortos@users.noreply.github.com>
2021-02-03 17:30:26 -0700

    size and ability scores
    Conflicts:
    	creatures/stats/size.py

--- creatures/stats/size.py
+++ creatures/stats/size.py
@@ -2,9 +2,24 @@
 from enum import Enum
 
 class Size (Enum):
-	Tiny = 0
-	Small = 1
-	Medium = 2
-	Large = 3
-	Huge = 4
-	Gargantuan = 5
+  Tiny = 0
+  Small = 1
+  Medium = 2
+  Large = 3
+  Huge = 4
+  Gargantuan = 5
+
+  @staticmethod
+  def get_side_length(sz):
+    s = 5
+    if sz == Size.Tiny:
+      s = 2.5
+    elif sz in [Size.Small, Size.Medium]:
+      s = 5
+    elif sz == Size.Large:
+      s = 10
+    elif sz == Size.Huge:
+      s = 15
+    elif sz == Size.Gargantuan:
+      s = 20
+    return s
